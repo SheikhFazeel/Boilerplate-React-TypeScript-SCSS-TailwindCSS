3b6f7b18770a914f20a215fc82fb534c2344609f
Fazeel <fazeel.ashraf@vadion.com>
2021-06-22 15:21:31 +0500

    Boilerplate for React + TypeScript + SCSS + Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+function App() {
+  return (
+    <div className="text-5xl text-center mt-4">
+       React
+    </div>
+  );
+}
+
+export default App;
